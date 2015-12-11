90d557406e0dc0968f602f8affb1a60e6bdd01ac
Gregory-Mathew P. Thalassenos <greg.thalassenos@gmail.com>
2015-12-11 09:59:41 -0500

    can you please help me

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -13,8 +13,7 @@ public class FracCalc {
     	Scanner scan = new Scanner(System.in); 
     	System.out.println("Fractions"); 
     	String input = scan.nextLine(); 
-    	System.out.println(produceAnswer(input)); 
-
+    	
     }
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
@@ -24,32 +23,62 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-   public static String splitFraction (String input)
-   { 	  	
-	String numerator = input.substring(input.lastIndexOf("/")-1);
-	String whole = input.substring(input.lastIndexOf("_")-1);
-	String denom= input.substring(input.lastIndexOf("/")+1);
-	String[] values = {};
+    
+    static String numerator = "";
+    static String denom = "";
+    static String whole = "";
+  
+    
+    public static String[] splitFraction (String input)
+   { 	
+	  
+	   if(input.contains("_")){
+	 numerator = input.substring(input.lastIndexOf("/")-1);
+	 whole = input.substring(input.lastIndexOf("_")-1);
+	 denom = input.substring(input.lastIndexOf("/")+1);
+	   } else {
+		   if(input.contains("/")){
+			   whole = "0";
+			   numerator = input.substring(input.lastIndexOf("/")-1);
+			   denom = input.substring(input.lastIndexOf("/")+1);
+					   
+		   } else {
+		   whole = input;
+		   numerator = "0";
+		   denom = "1";
+		   
+		   } 
+		   
+	 }
+		   
+	String[] values = new String[3];
+	values[0] = whole;
+	values[1] = denom;
+	values[2] = numerator;
 	return values;
    }
     public static String produceAnswer(String input)
     { 
     	
-    	
+    	System.out.println(produceAnswer(input)); 
+
     	
     	String denom = input.substring(input.lastIndexOf("/")+1);
     	String whole = input.substring(input.lastIndexOf("_"));
     	String numerator = input.substring(input.lastIndexOf("/")-1);
     	
-       /* String secfrac = scanned.substring(scanned.lastIndexOf(" ")+1);
+     /*  String secfrac = scanned.substring(scanned.lastIndexOf(" ")+1);
     	String first= scanned.substring(0,secfrac.indexOf("_"));
-    	String denominator = scanned.substring(1,secfrac.lastIndexOf() );*/
+    	String deno minator = scanned.substring(1,secfrac.lastIndexOf() );*/
 
-    	
-    	String[] answers = {"whole","numerator" ,"denominator"};
+    	String[] values = new String[3];
+    	values[0] = whole;
+    	values[1] = denom;
+    	values[2] = numerator;
+    	String[] answers = {whole,numerator ,denom};
         // TODO: Implement this function to produce the solution to the input
         
-        return "whole:" + numerator ;
+        return "whole:" + values[0] + "Numerator: " + values[1] + "Denominator: " + values[2];
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
